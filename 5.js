24e72f093c4d9b66fe88714398ad6368b680dc81
Aleksandr970224 <acceptf970224@gmail.com>
2023-02-14 18:34:20 +0300

    completed tasks

---
+++ 5.js
@@ -0,0 +1,77 @@
+/* Задание 1
+Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+
+Вам понадобятся методы строк. */
+
+let ucFirst = (str) => {
+  let result;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase() && str[i] !== ' ' && str[i] !== ',') {
+      result = str[i];      
+      return result;
+    };
+  };
+};
+
+let string = 'hello, World';
+console.log(ucFirst(string));
+
+
+
+/* Задание 1
+Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+
+Вам понадобятся методы строк. */
+
+let ucFirst = (str) => {
+  let result;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase() && str[i] !== ' ' && str[i] !== ',') {
+      result = str[i];
+      return result;
+    };
+  };
+};
+
+let string = 'hello, World';
+console.log(ucFirst(string));
+
+
+
+/* Задание 1
+Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+
+Вам понадобятся методы строк. */
+
+let ucFirst = (str) => {
+  let result;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase() && str[i] !== ' ' && str[i] !== ',') {
+      result = str[i];
+      return result;
+    };
+  };
+};
+
+let string = 'hello, World';
+console.log(ucFirst(string));
+
+
+
+/* Задание 1
+Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+
+Вам понадобятся методы строк. */
+
+let ucFirst = (str) => {
+  let result;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase() && str[i] !== ' ' && str[i] !== ',') {
+      result = str[i];
+      return result;
+    };
+  };
+};
+
+let string = 'hello, World';
+console.log(ucFirst(string));
